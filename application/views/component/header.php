acb6cd4b388bbf9721ae7c4ab4fef3879c381538
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-03 15:04:08 +0700

    Add Header Component

---
+++ application/views/component/header.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Umami</title>
+	
+    <!-- Import Bootsrap 4.1.3 -->
+	<link href="<?php echo base_url()?>/assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="<?php echo base_url()?>/assets/css/style.css" rel="stylesheet" />
+	<script src="<?php echo base_url()?>/assets/js/jquery-3.3.1.min.js"></script>
+	<script src="<?php echo base_url()?>/assets/js/bootstrap.min.js"></script>
+</head>
+<body>

8cc823e2e94ab48d0e1a1ea75f978c87a85f35dc
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-03 15:04:19 +0700

    Add Navbar Component

---
+++ application/views/component/navbar.php
@@ -0,0 +1,20 @@
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Course <span class="caret"></span></a>
+      </ul>
+    <form class="navbar-form navbar-right" action="/action_page.php">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Cari" name="search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+    </div>
+  </div>
+</nav>

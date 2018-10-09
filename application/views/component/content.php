c2e981d61b5082cc65ef69e9ed7118b31d00d429
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-09 23:05:43 +0700

    Add Content View

---
+++ application/views/component/content.php
@@ -0,0 +1,34 @@
+  <!-- header -->
+  <header>
+        <h1 style="font-size: 50px; color:white;">Unduh Materi-Materi IT!</h1>
+      <form>
+        <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=50px  style="border-radius: 5px">
+        <button>
+          <i class="fa fa-search"></i>
+        </button>
+      </form>
+          
+    </header>
+      
+  <!-- Body -->
+  <div class="container bg-4 text-left" style="margin-bottom: 20px;">  
+      <h3 style=" text-indent: 50px;">Semester 1</h3><br> 
+    <div id="tabel">
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+    </div>   
+      <h3 style=" text-indent: 50px;">Semester 2</h3><br> 
+    <div id="tabel">
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+      <div>Language</div>
+    </div>  
+    </div>
+    <br/>

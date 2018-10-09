3eb99fec3caf90faa7179c50ca1fc9c4a01f7393
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-09 23:56:22 +0700

    Add List Materi View

---
+++ application/views/main-content/list-materi.php
@@ -0,0 +1,22 @@
+<section>
+  <h3>Matematika Diskrit</h3>
+  <h4>Dosen : Akik Hidayat</h4>
+  <div id="tabel">
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+  </div>
+  <div class="pagination">
+    <a href="#">&laquo;</a>
+    <a href="#">1</a>
+    <a href="#">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#">5</a>
+    <a href="#">6</a>
+    <a href="#">&raquo;</a>
+  </div>
+</section>			

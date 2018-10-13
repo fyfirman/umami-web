809408bd8b4ac79eda5fe5568f75ee0c477164de
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-13 08:42:29 +0700

    Add coment and fix indentation

--- application/views/component/navbar.php
+++ application/views/component/navbar.php
@@ -1,24 +1,29 @@
 <!-- Navbar -->
-<div class="container">
-  <nav style="">
-      <ul class="navtampil">
-        <li class="nav-item navbar-left">
-            <div class="dropdown">
-            <span style="color: #000099">Course</span>
-            <div class="dropdown-content">
-              <p>Course 1</p>
-              <p>Course 2</p>
-              <p>Course 3</p>
-            </div>
-          </div>
-      </li>
-        <li class="nav-item" ><a id="coba">Umami</a></li>
-        <form class="navbar-form navbar-right" action="/action_page.php">
-          <input type="text" name="search" placeholder="Cari" style="border-radius: 2px;">
-        <button>
-          <i class="fa fa-search"></i>
-        </button>
-        </form>
-      </ul>
-    </nav>
-</div>
+<nav class="container">
+  <ul class="navtampil">
+
+    <!-- Dropdown menu -->
+    <li class="nav-item navbar-left">
+        <div class="dropdown">
+        <span style="color: #000099">Course</span>
+        <div class="dropdown-content">
+          <p>Course 1</p>
+          <p>Course 2</p>
+          <p>Course 3</p>
+        </div>
+      </div>
+    </li>
+
+    <!-- Logo -->
+    <li class="nav-item" ><a id="coba">Umami</a></li>
+    
+    <!-- Searchbar -->
+    <form class="navbar-form navbar-right" action="/action_page.php">
+      <input type="text" name="search" placeholder="Cari" style="border-radius: 2px;">
+      <button>
+        <i class="fa fa-search"></i>
+      </button>
+    </form>
+    
+  </ul>
+</nav>

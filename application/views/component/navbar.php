0d15fa6bbe5eb7e334627c9b9825ceed703a2383
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-13 13:53:55 +0700

    Add logo umami on navbar

--- application/views/component/navbar.php
+++ application/views/component/navbar.php
@@ -5,7 +5,7 @@
     <!-- Dropdown menu -->
     <li class="nav-item navbar-left">
         <div class="dropdown">
-        <span style="color: #000099">Course</span>
+        <span style="color: #1060f2">Course</span>
         <div class="dropdown-content">
           <p>Course 1</p>
           <p>Course 2</p>
@@ -15,7 +15,7 @@
     </li>
 
     <!-- Logo -->
-    <li class="nav-item" ><a id="coba">Umami</a></li>
+    <li class="nav-item" ><img src="<?php echo base_url()?>/assets/img/umami-logo.png" style="width: 80px"></li>
     
     <!-- Searchbar -->
     <form class="navbar-form navbar-right" action="/action_page.php">
@@ -24,6 +24,6 @@
         <i class="fa fa-search"></i>
       </button>
     </form>
-    
+
   </ul>
 </nav>

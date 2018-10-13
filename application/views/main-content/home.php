d0ee495af953f884788c23d3350366b82aee9420
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-13 10:47:43 +0700

    menambahkan looping pada materi, gridding, menghapus id tabel

--- application/views/main-content/home.php
+++ application/views/main-content/home.php
@@ -1,34 +1,44 @@
-  <!-- header -->
-  <header>
-        <h1 style="font-size: 50px; color:white;">Unduh Materi-Materi IT!</h1>
-      <form>
-        <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=50px  style="border-radius: 5px">
-        <button>
-          <i class="fa fa-search"></i>
-        </button>
-      </form>
-          
-    </header>
-      
-  <!-- Body -->
-  <div class="container bg-4 text-left" style="margin-bottom: 20px;">  
-      <h3 style=" text-indent: 50px;">Semester 1</h3><br> 
-    <div id="tabel">
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-    </div>   
-      <h3 style=" text-indent: 50px;">Semester 2</h3><br> 
-    <div id="tabel">
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-      <div>Language</div>
-    </div>  
+<!-- header -->
+<header>
+    <h1 style="font-size: 50px; color:white;">Unduh Materi-Materi IT!</h1>
+    <form>
+      <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=50px  style="border-radius: 5px">
+      <button>
+        <i class="fa fa-search"></i>
+      </button>
+    </form>
+        
+  </header>
+    
+<!-- Body -->
+<div class="container bg-4 text-left" style="margin-bottom: 20px;">  
+  <?php for ($i=0; $i < 7; $i++): ?>
+    <div class="list-materi row">
+      <h3>Semester <?php echo $i+1?></h3>
+      <div>
+          <div class="row">
+            <a href="#">
+                <div class="materi col-md-4">Language</div>
+            </a>
+            <a href="#">
+              <div class="materi col-md-4">Language</div>
+            </a>  
+            <a href="#">
+              <div class="materi col-md-4">Language</div>
+            </a>   
+          </div>
+          <div class="row">
+            <a href="#">
+                <div class="materi col-md-4">Language</div>
+            </a>
+            <a href="#">
+              <div class="materi col-md-4">Language</div>
+            </a>  
+            <a href="#">
+              <div class="materi col-md-4">Language</div>
+            </a>
+          </div>
+      </div>   
     </div>
-    <br/>
+  <?php endfor ?>
+</div>

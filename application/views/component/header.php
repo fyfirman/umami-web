80392d2bc90756f59c4ba66a5a74f759c087b75c
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-22 16:15:15 +0700

    Delete uneccessary comment

--- application/views/component/header.php
+++ application/views/component/header.php
@@ -1,25 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Umami</title>
-  
-  Import Bootsrap 4.1.3
-  <link href="<php echo base_url()?>/assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="<php echo base_url()?>/assets/css/style.css" rel="stylesheet" />
-  <script src="<php echo base_url()?>/assets/js/jquery-3.3.1.min.js"></script>
-  <script src="<php echo base_url()?>/assets/js/bootstrap.min.js"></script>
-</head>
-<body> -->
 
 <html>
 <head>
   <title>Umami</title>
   <meta charset="utf-8">
-  <link href="<?php echo base_url()?>/assets/css/style.css" rel="stylesheet" />
+  <link href="<?php echo base_url()?>/assets/css/
+  .css" rel="stylesheet" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>

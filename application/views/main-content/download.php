5ba48f0f5053809884c14c60480705710eb02c1a
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-30 18:00:22 +0700

    change position on the download button

--- application/views/main-content/download.php
+++ application/views/main-content/download.php
@@ -1,24 +1,17 @@
 <!-- header -->
 <header>
-	<div class="container">
-	    <h1 style="font-size: 50px; color:white; text-align: left;">01 Pengantar DB Terdistribusi.pptx</h1>
-	    <p style="color:white; text-align: left;">Mata Kuliah: Sistem Database II</p>
-    </div>
-    <br/><br/>
-    
-    <div class="button1">
-    	<div>
-	        <div class="row" align="center">
-	            <a href="#">
-	                <div>
-	                	<div class="download-button m-2 col-md-2" style=" background: rgba(0,0,0,0); color:white;">Download</div>
-	                </div>
-	            </a>
-        	</div>
-    	</div>
+	<div class="container" style="margin-bottom: 50px;">
+		<h1 style="font-size: 50px; color:white; text-align: left;">01 Pengantar DB Terdistribusi.pptx</h1>
+		<p style="color:white; text-align: left;">Mata Kuliah: Sistem Database II</p>
+	</div>
+
+	<a href="#" class="download-button" style=" background: rgba(0,0,0,0); color:white; ">
+		Download
+	</a>
 </header>
 
 
 <div class="content">
-<div class=" container" style="margin-bottom: 20px;">  
-</div>
+	<div class=" container">  
+	</div>
+

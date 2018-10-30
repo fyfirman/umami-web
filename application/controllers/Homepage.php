fe6411206517d90e09dd25b9751da5167651ce93
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-30 10:12:52 +0700

    Create user model and get data through controller

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -2,11 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Homepage extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('user_model');
+	}
+
 	public function index()
 	{
+		$data['user'] = $this->user_model->get_data()->result();
 		$this->load->view('component/header.php');
 		$this->load->view('component/navbar.php');
-		$this->load->view('main-content/home.php');
+		$this->load->view('main-content/home.php',$data);
 		$this->load->view('component/footer.php');
 	}
 }

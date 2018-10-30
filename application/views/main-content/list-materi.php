015ea6d2e2911bf25747d2cc1519a69d72a6ebcb
faradilla <faradillaazra@gmail.com>
2018-10-30 15:24:38 +0700

    add more boxes
    in xampp localhost

--- application/views/main-content/list-materi.php
+++ application/views/main-content/list-materi.php
@@ -1,4 +1,5 @@
 <section>
+  <div class="container bg-4 text-left" style="margin-bottom: 0px; padding-bottom: 30px;">  
   <h3>Matematika Diskrit</h3>
   <h4>Dosen : Akik Hidayat</h4>
   <div id="tabel">
@@ -8,15 +9,25 @@
     <div><a href="url">Language</a></div>
     <div><a href="url">Language</a></div>
     <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
+    <div><a href="url">Language</a></div>
   </div>
-  <div class="pagination">
-    <a href="#">&laquo;</a>
-    <a href="#">1</a>
-    <a href="#">2</a>
-    <a href="#">3</a>
-    <a href="#">4</a>
-    <a href="#">5</a>
-    <a href="#">6</a>
-    <a href="#">&raquo;</a>
-  </div>
-</section>			
+  <div class="center">
+    <br>
+    </br>
+    <div class="pagination">
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+    </div>
+  </div>  
+</section>

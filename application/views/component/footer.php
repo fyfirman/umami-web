d5dd8bcb99bde7accef92c7c208da76a35f99545
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-30 20:05:09 +0700

    add a border to the footer, and change the background in the footer, body, and navbar

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -1,6 +1,9 @@
   <!-- footer -->
   <footer>
+  	<div class="gradient">
+  	</div>
     <div class="footer-logo"><img src="<?php echo base_url()?>/assets/img/umami-logo.png" style="width: 80px"><span class="copyright">copyright &copy; 2018</span></div>
+
   </footer>
 
   <div class="footer-color">

4f02506b107bffe659e8fd8f0b68d53fe56fb43e
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-10-30 17:53:48 +0700

    add a background under the footer

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -3,8 +3,8 @@
     <div class="footer-logo"><img src="<?php echo base_url()?>/assets/img/umami-logo.png" style="width: 80px"><span class="copyright">copyright &copy; 2018</span></div>
   </footer>
 
-  <div class="">
- 
+  <div class="footer-color">
+
   </div>
 </body>
 </html>

30f9cdff54647cc48604d8c02b2e2bafed1b6efd
Firmansyah Yanuar <fyfirman@gmail.com>
2018-10-31 11:47:52 +0700

    Add bootstrap js

--- application/views/component/header.php
+++ application/views/component/header.php
@@ -9,6 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <!-- Script -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="<?php echo base_url()?>assets/js/loading-spinner.js""></script>
 
   <!-- Style -->

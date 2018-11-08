059f3b52e1c906676d69558b7d8499c796e2244c
Firmansyah Yanuar <fyfirman@gmail.com>
2018-11-08 20:13:53 +0700

    Modify Login Page

--- application/views/component/header.php
+++ application/views/component/header.php
@@ -15,6 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <!-- Style -->
   <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet" />
   <link href="<?php echo base_url()?>assets/css/loading-spinner.css" rel="stylesheet" />
+  <link href="<?php echo base_url()?>assets/css/login.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
--- application/views/main-content/login.php
+++ application/views/main-content/login.php
@@ -1,5 +1,5 @@
-<div class="content">
-    <h1> LOGIN </h1>
+<!-- <div class="content">
+	<h1> LOGIN </h1>
 	<form action="action_page.php">
 		<div class="container">
 			<center>
@@ -19,4 +19,30 @@
 			<span class="psw"><a href="#">Forgot password?</a></span>
 		</div>
 	</form>
+</div> -->
+
+<div class="wrapper fadeInDown">
+	<div id="formContent">
+		<!-- Tabs Titles -->
+		<h2 class="signMenu active">Login</h2>
+		<h2 class="signMenu inactive underlineHover">Register</h2>
+
+		<!-- Icon -->
+		<div class="fadeIn first">
+			<!-- <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> -->
+		</div>
+
+		<!-- Login Form -->
+		<form class="login">
+			<input type="text" id="login" class="fadeIn second signForm" name="login" placeholder="Username">
+			<input type="password" id="password" class="fadeIn third signForm" name="login" placeholder="Password">
+			<input type="submit" id="signButton" class="fadeIn fourth" value="Log In">
+		</form>
+
+		<!-- Remind Passowrd -->
+		<div id="formFooter">
+			<a href="#">Forgot Password?</a>
+		</div>
+
+	</div>
 </div>

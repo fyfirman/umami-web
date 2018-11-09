fc92afc1d57646eca1b08fbcc8e364f1289cfc62
Firmansyah Yanuar <fyfirman@gmail.com>
2018-11-09 15:13:56 +0700

    Add favicon

--- application/views/component/header.php
+++ application/views/component/header.php
@@ -7,12 +7,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <title>Umami</title>
   <meta charset="utf-8">
 
+
   <!-- Script -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="<?php echo base_url()?>assets/js/loading-spinner.js""></script>
 
   <!-- Style -->
+  <link href="<?php echo base_url()?>assets/img/umami-icon.png" rel="icon" />
   <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet" />
   <link href="<?php echo base_url()?>assets/css/loading-spinner.css" rel="stylesheet" />
   <link href="<?php echo base_url()?>assets/css/login.css" rel="stylesheet" />

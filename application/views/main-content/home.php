4048d3aba93290b1dfa9f86349344fe5c7ec0414
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-11-09 13:56:27 +0700

    Add margin between materi-item and move search button

--- application/views/main-content/home.php
+++ application/views/main-content/home.php
@@ -2,8 +2,8 @@
 <header>
     <h1 style="font-size: 72px; color:white;">Unduh Materi-Materi IT!</h1>
     <form>
-      <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=75px  style="height: 45px; text-indent: 15px;">
-      <button style="height: 45px">
+      <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=75px  style="height: 48px; text-indent: 15px;">
+      <button class="search-button"style="height: 45px">
         <i class="fa fa-search"></i>
       </button>
     </form>
@@ -12,31 +12,31 @@
     
 <!-- Body -->
 <div class="content">
-<div class=" container">  
+<div class="container"> 
   <?php for ($i=0; $i < 7; $i++): ?>
     <div class="list-materi row">
       <h3>Semester <?php echo $i+1?></h3>
       <div>
           <div class="row">
-            <a href="#">
-                <div class="materi m-2 col-md-4">Language</div>
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
             </a>
-            <a href="#">
-              <div class="materi m-2 col-md-4">Language</div>
-            </a>  
-            <a href="#">
-              <div class="materi m-2 col-md-4">Language</div>
-            </a>   
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
+            </a>
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
+            </a>
           </div>
           <div class="row">
-            <a href="#">
-                <div class="materi col-md-4">Language</div>
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
             </a>
-            <a href="#">
-              <div class="materi col-md-4">Language</div>
-            </a>  
-            <a href="#">
-              <div class="materi col-md-4">Language</div>
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
+            </a>
+            <a class="col-md-4" href="#">
+                <div class="materi" style="">Language</div>
             </a>
           </div>
       </div>   

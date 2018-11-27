7bbda1973aa40075330573ac6a76595e203e0cc9
Firmansyah Yanuar <fyfirman@gmail.com>
2018-11-28 00:38:28 +0700

    Rework UI

--- application/views/main-content/home.php
+++ application/views/main-content/home.php
@@ -1,12 +1,14 @@
 <!-- header -->
-<header>
-  <h1 style="">Unduh Materi-Materi IT!</h1>
-  <form>
-    <input type="text" name="search" placeholder="Apa yang Kamu Cari?" size=75px  style="">
-    <button class="search-button"style="height: 45px">
-      <i class="fa fa-search"></i>
-    </button>
-  </form>
+<header class="header col-md-12">
+  <div class="container">
+    <h1 style="">Unduh Materi-Materi IT!</h1>
+    <form >
+      <input class="search-input" type="text" name="search" placeholder='Apa yang Kamu Cari?'>
+      <button class="search-button" style="height: 43px">
+        <i class="fa fa-search"></i>
+      </button>
+    </form>
+  </div>
 </header>
     
 <!-- Body -->
@@ -18,7 +20,7 @@
       <div class="row">
         <?php foreach ($matkul as $obj):?>
           <?php if ($obj['semester'] == $i): ?>
-            <a class="col-md-4" href="#">
+            <a class="col-md-4" href="<?php echo base_url()?>materi">
                 <div class="materi" style=""><?php echo $obj['nama_matkul']; ?></div>
             </a>
           <?php endif ?>

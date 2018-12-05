d46150ccc13cc618df7ac3102df42e8654eb75d8
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2018-12-05 12:13:12 +0700

    Fix navbar on mobile

--- application/views/component/navbar.php
+++ application/views/component/navbar.php
@@ -1,20 +1,21 @@
 <!-- Navbar -->
-<nav class="container">
-  <ul class="navtampil">
-
-    <div class="col-md-4">
-      <li class="nav-item navbar-left">
+<nav class="container nav-desktop">
+  <!-- Left -->
+   <div id="hello-message" class="col-md-4">
         <?php $login = true; ?> <!-- debug code -->
         <?php if($login):?>
           Halo fyfirman!
         <?php else: ?>
           <a href="<?php echo base_url()?>login">Login</a>
         <?php endif ?>
-      </li>
     </div>
     <!-- Logo -->
-    <div class="col-md-4">
-      <li class="nav-item"><a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>/assets/img/umami-logo.png" style="width: 80px"></a></li>
+    <div id="brand-logo" class="col-md-4 col-xs-6">
+      <a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>/assets/img/umami-logo.png" style="width: 80px"></a>
     </div>
-  </ul>
+
+     <!-- logout -->
+    <div id="log" class="col-md-4 col-xs-6">
+      <a href="">Logout <i class="fa fa-sign-out"></i></a>
+    </div>
 </nav>

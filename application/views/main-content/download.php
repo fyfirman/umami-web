26541df0c5f35b05962352b4ba196487cc2d32fa
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-07 16:51:11 +0700

    Button download hanya bisa diakses yang telah login

--- application/views/main-content/download.php
+++ application/views/main-content/download.php
@@ -1,3 +1,5 @@
+<?php $login = $this->session->userdata('status');?>
+
 <!-- header -->
 <header class="header col-md-12">
 	<div class="container" style="margin-bottom: 50px;">
@@ -5,8 +7,8 @@
 		<p style="color:white; text-align: left;">Mata Kuliah: Sistem Database II</p>
 	</div>
 
-	<a href="<?php echo $materi->link_download ?>" class="download-button" style=" background: rgba(0,0,0,0); color:white; ">
-		Download
+	<a href="<?php if($login) echo $materi->link_download; else echo base_url('login	');?>" class="download-button" style=" background: rgba(0,0,0,0); color:white; ">
+		<?php if($login) echo "Unduh"; else echo "Kamu belum login!";?>
 	</a>
 </header>
 
@@ -17,12 +19,12 @@
 		<?php for($i=0; $i<5; $i++): ?>
 		<a href="#">
 		    <div class="File col-md-2" style="margin-left:0;">
-		     	<div align="center" style="margin-top:10px;">
-		      		<div class="circle" >
+		    	<div align="center" style="margin-top:10px;">
+					<div class="circle" >
 		        		<h2 style="margin-top: 35px;">PDF</h2>
-		      		</div>
-		      	</div>
-		      	<h4 style="text-align: center; margin-top: 30px; color:#727272;">Nama-Fileeeee....</h4>
+					</div>
+				</div>
+				<h4 style="text-align: center; margin-top: 30px; color:#727272;">Nama-Fileeeee....</h4>
 		    </div>
 		</a>
 		<?php endfor ?>

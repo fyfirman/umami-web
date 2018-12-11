37df38e42ada7c00e7c5ee413016875a468de598
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-12 00:47:57 +0700

    Add register success message

---
+++ application/views/main-content/register_success.php
@@ -0,0 +1,10 @@
+<div class="wrapper fadeInDown">
+	<div id="formContent">
+		<!-- Tabs Titles -->
+		<a href="login"><h2 class="signMenu inactive underlineHover">Login</h2></a>
+		Register Sukses! 
+
+	</div>
+</div>
+
+<script src="<?php echo base_url()?>assets/js/confirm-password.js""></script>
